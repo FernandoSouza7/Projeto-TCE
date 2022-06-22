91dfca59e1ad72d71d0193c144754dc7651f92f8
Matheus Fernando <matheus.fernando.coder@gmail.com>
2022-06-22 14:52:13 -0300

    Second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmployesComponent } from './component/employes/employes.component';
 import { LoginComponent } from './component/login/login.component';
 import { NewemployeComponent } from './component/newemploye/newemploye.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path : 'login',
     component: LoginComponent
+  },
+  {
+    path : 'sidebar',
+    component: SidebarComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EmployesComponent } from './component/employes/employes.component';
 import { NewemployeComponent } from './component/newemploye/newemploye.component';
 import { LoginComponent } from './component/login/login.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployesComponent,
     NewemployeComponent,
-    LoginComponent
+    LoginComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
